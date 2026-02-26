4e17846953a2a64ad515ab39392a434d95b24660
Thanadon.aon <aon59079@gmail.com>
2026-02-26 10:27:06 +0700

    formselectfiles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Router2 from './router2';
 import Router3 from './router3';
 import FormBootstrap from './form-bootstrap';
 import FormText from './form-text';
+import FormSelectFile from './from-select-file';
 export default function App() {
-  return <FormText />;
+  return <FormSelectFile />;
 }   
